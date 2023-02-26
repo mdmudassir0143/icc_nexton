267e2f733b56bed81aa2f79be9dcd2fdce5896b2
Mohammad Mudassir <mdmudassir0143@gmail.com>
2023-02-26 23:13:30 +0530

    Backend Created

---
+++ Frontend/src/slug/Engage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./engage.css"
+
+export default function Engage() {
+  return <div className="container">
+    <div className="modal">
+      Modals
+    </div>
+    <div className="chant">
+      Chants
+    </div>
+
+  </div>;
+}
--- src/slug/Engage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Engage() {
-  return <div>Engage</div>;
-}
