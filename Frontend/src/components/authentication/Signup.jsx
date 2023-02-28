cbd69a8656fb85a2e15365107e8cf2156ac3018d
mdozairq <mdozairq@gmail.com>
2023-02-28 13:55:11 +0530

    fix(auth): added new layout for authentication

---
+++ Frontend/src/components/authentication/Signup.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './Signup.css';
+
+function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+  };
+
+  return (
+    <div className="signup-container">
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Signup</button>
+      </form>
+      <p>Already have an account? <a href="/">Login here</a></p>
+    </div>
+  );
+}
+
+export default Signup;
